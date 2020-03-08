d900c0d95826e8744aaab241cd5c5a57fdf9e7e6
Shota Tamura <tamura@yuuniworks.com>
2020-03-08 17:19:16 +0900

    server

---
+++ server.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const fs = require("fs");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { ApolloServer, gql } = require("apollo-server-express");
+const cors = require("cors");
+
+const typeDefs = fs
+  .readFileSync(path.join(__dirname, "schema.graphql"))
+  .toString();
+
+const resolvers = {
+  Query: {
+    user() {
+      return { name: "hoge", id: "hoge" };
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.use(cors());
+app.use(bodyParser.json());
+app.listen(3001);
